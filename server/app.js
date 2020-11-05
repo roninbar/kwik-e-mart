4ec0996d6c0b76eaa3c27cdd091da435f17a7a05
ron <rinbar70@gmail.com>
2020-11-05 23:08:43 +0200

    Remove unused routers.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,7 @@
-var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
+var express = require('express');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var path = require('path');
 
 var app = express();
 
@@ -13,8 +10,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
