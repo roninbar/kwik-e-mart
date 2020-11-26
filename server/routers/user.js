74ca20fd5fd90a24d83a9ba5a0b8417fb4e6914d
ron <rinbar70@gmail.com>
2020-11-26 23:06:48 +0200

    Use user ID (9 decimal digits) as collection _id.

--- server/routers/user.js
+++ server/routers/user.js
@@ -1,24 +1,11 @@
 const User = require('../models/User');
-const express = require('express');
+const { Router } = require('express');
 
-const router = new express.Router();
+const router = new Router();
 
-router.post('/', async function ({ body: { id, username, password, firstName, lastName, city, streetAddress } }, res) {
-    const user = new User({
-        id,
-        role: 'user',
-        username,
-        password,
-        name: {
-            first: firstName,
-            last: lastName,
-        },
-        address: {
-            city,
-        },
-        streetAddress,
-    });
-    const { _id } = await user.save();
+router.post('/', async function ({ body: { id: _id, username, password, firstName, lastName, city, streetAddress } }, res) {
+    const user = new User({ _id, role: 'user', username, password, firstName, lastName, city, streetAddress });
+    await user.save();
     return res.location(`/api/user/${_id}`).sendStatus(201);
 });
 
