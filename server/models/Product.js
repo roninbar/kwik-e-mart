58093ddec9a997d57cbbb085c6609d8fa3cb7e46
ron <rinbar70@gmail.com>
2020-11-27 02:53:54 +0200

    Implement "POST /api/category/:categoryId/product" (create a new product in category).

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,12 +1,10 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const schema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
+module.exports = model('Product', new Schema({
+    name: { type: String, required: true, unique: true, trim: true },
     price: { type: Number, required: true, min: 0 },
-    image: { type: String, required: true },
+    imageUrl: { type: String, required: true },
 }, {
     timestamps: true,
-});
-
-module.exports = mongoose.model('Product', schema);
+}));
 
--- server/routers/category.js
+++ server/routers/category.js
@@ -1,4 +1,5 @@
 const ProductCategory = require('../models/ProductCategory');
+const Product = require('../models/Product');
 const { Router } = require('express');
 
 const router = new Router();
@@ -10,6 +11,29 @@ router.post('/', async function ({ originalUrl, body: { name } }, res) {
         return res.location(`${originalUrl}/${_id}`).sendStatus(201);
     } catch ({ code }) {
         return res.sendStatus(code === 11000 ? 409 : 500);
+    }
+});
+
+const productRouter = new Router();
+
+router.use(
+    '/:categoryId/product',
+    async function (req, res, next) {
+        const { params: { categoryId } } = req;
+        req.category = await ProductCategory.findById(categoryId);
+        return req.category ? next() : res.sendStatus(404);
+    },
+    productRouter,
+);
+
+productRouter.post('/', async function ({ originalUrl, category, body: { name, price, imageUrl } }, res) {
+    try {
+        const product = new Product({ name, price, imageUrl });
+        const { _id } = await product.save();
+        await category.update({ $push: { products: product } });
+        return res.set('Content-Location', `${originalUrl}/${_id}`).status(201).json(product);
+    } catch ({ code, message }) {
+        return res.status(code === 11000 ? 409 : 400).send(message);
     }
 });
 
