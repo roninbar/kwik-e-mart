341d82bfc105175b8c7e06bc4195ea3f90e5627e
ron <rinbar70@gmail.com>
2020-12-09 23:45:22 +0200

    Change the implementation of the one-to-many relationship between ProductCategory and Product.

--- models/Product.js
+++ models/Product.js
@@ -4,6 +4,7 @@ module.exports = model('Product', new Schema({
     name: { type: String, required: true, unique: true, trim: true },
     price: { type: Number, required: true, min: 0 },
     imageUrl: { type: String, required: true },
+    categoryId: { type: Schema.Types.ObjectId, ref: 'ProductCategory' },
 }, {
     timestamps: true,
 }));
