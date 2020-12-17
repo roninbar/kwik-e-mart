cfe24b56e4a2a127a257f3f72e8b78cf684e5224
ron <rinbar70@gmail.com>
2020-12-17 14:52:26 +0200

    Add missing MatButtonModule (accidentally removed in commit 5c2ecb9).

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -26,6 +27,7 @@ import { ShoppingPageComponent } from './shopping-page/shopping-page.component';
     FormsModule,
     HttpClientModule,
     LayoutModule,
+    MatButtonModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
