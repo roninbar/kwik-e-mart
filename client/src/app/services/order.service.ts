ec433bcb72f3b583c884928ec9b97d483da72dee
ron <rinbar70@gmail.com>
2020-12-21 12:16:58 +0200

    Simplify placeOrderRx.

--- client/src/app/services/order.service.ts
+++ client/src/app/services/order.service.ts
@@ -9,11 +9,7 @@ export class OrderService {
 
   constructor(private http: HttpClient) { }
 
-  placeOrderRx(fields: { [key: string]: string | number }): Observable<string> {
-    const body = Object.entries(fields).reduce(
-      (params: HttpParams, [param, value]) => params.append(param, String(value)),
-      new HttpParams()
-    );
-    return this.http.post<string>('/api/order', body);
+  placeOrderRx(fields: { [key: string]: string }): Observable<string> {
+    return this.http.post<string>('/api/order', new HttpParams({ fromObject: fields }));
   }
 }
