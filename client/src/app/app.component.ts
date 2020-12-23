f6ba7befa0a896ece998cbadcaea8c6896c2fd81
ron <rinbar70@gmail.com>
2020-12-23 15:37:57 +0200

    Add a common toolbar to all the pages.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Shop';
+  title = 'Kwik-E-Mart';
+
+  constructor(private router: Router) { }
+
+  async goToHomePage(): Promise<boolean> {
+    return await this.router.navigateByUrl('/');
+  }
 }
