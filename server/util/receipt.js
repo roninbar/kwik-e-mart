4422fbb6f64e4e909556dc1db612252db6486f23
ron <rinbar70@gmail.com>
2020-12-29 22:50:24 +0200

    Add the total number of items to the receipt.

--- server/util/receipt.js
+++ server/util/receipt.js
@@ -5,11 +5,12 @@ const path = require('path');
 async function generateReceipt(order, dir) {
     await fs.mkdir(dir, { recursive: true });
     const rows = order.items.map(({ product: { name, price }, quantity }, idx) => `${idx + 1} | ${name} | ${quantity} | ${price} | ${quantity * price}`).join('\n');
-    const total = order.items.reduce((sum, { product: { price }, quantity }) => sum + quantity * price, 0);
+    const totalItems = order.items.reduce((sum, { quantity }) => sum + quantity, 0);
+    const totalPrice = order.items.reduce((sum, { product: { price }, quantity }) => sum + quantity * price, 0);
     const markdown = `\\# | Product | Quantity | Price | Total
 -|-|-|-|-
 ${rows}
-| **Total:** | | | **${total}**`;
+| **Total:** | **${totalItems}** | | **${totalPrice}**`;
     await fs.writeFile(path.join(dir, `${order._id}.md`), markdown);
     const pdf = await mdToPdf({ content: markdown }, { dest: path.join(dir, `${order._id}.pdf`) });
     return { markdown, pdf };
