c88be1906e0bf06e173ecdae23dcee3bcb2ba1bf
ron <rinbar70@gmail.com>
2021-01-02 12:22:19 +0200

    Add all the relevant info to the receipt.

--- util/receipt.js
+++ util/receipt.js
@@ -7,21 +7,66 @@ const log = debug('server:receipt');
 
 async function generateReceipt(order) {
     await order.populate('items.product').execPopulate();
+    const {
+        _id,
+        createdAt,
+        customer: {
+            _id: customerId,
+            name: {
+                first: firstName,
+                last: lastName,
+            },
+        },
+        delivery: {
+            to: {
+                house,
+                street,
+                city,
+            },
+            on: deliveryDate,
+        },
+        totalItems,
+        total,
+        payment: {
+            cc: {
+                number: ccnumber,
+            },
+        },
+    } = order;
     const dir = path.join(global.staticFilesDir, 'receipts');
     await fs.mkdir(dir, { recursive: true });
     const rows = order.items
         .map(({ product: { name, price }, quantity }, idx) => `| ${idx + 1} | ${name} | ${quantity} | &euro;${price.toFixed(2)} | &euro;${(quantity * price).toFixed(2)} |`).join('\n');
-    const totalItems = order.items.reduce((sum, { quantity }) => sum + quantity, 0);
-    const totalPrice = order.items.reduce((sum, { product: { price }, quantity }) => sum + quantity * price, 0);
-    const markdown = `| # | Product | Quantity | Price | Total |
+
+    const markdown = `# Kwik-E-Mart
+**${createdAt.toLocaleString('en-GB')}**  
+
+## Order #${_id}
+
+### Customer ${customerId}
+**${firstName} ${lastName}**  
+${street} ${house}  
+${city}  
+
+### Deliver On
+${deliveryDate.toLocaleDateString('en-GB', { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric' })}  
+
+| # | Product | Quantity | Price | Total |
 |-|-|-|-|-|
 ${rows}
-| | **Total:** | **${totalItems}** | | **&euro;${totalPrice.toFixed(2)}** |`;
+| | **Total:** | **${totalItems}** | | **&euro;${total}** |
+
+### Paid By Credit Card
+CC Number: \`${ccnumber}\`  
+
+***Thank you! Come again!***`;
+
     log(markdown);
     await fs.writeFile(path.join(dir, `${order._id}.md`), markdown);
     log('Generating PDF...');
     const pdf = await mdToPdf({ content: markdown }, { dest: path.join(dir, `${order._id}.pdf`) });
     log('done.');
+
     return { markdown, pdf };
 }
 
