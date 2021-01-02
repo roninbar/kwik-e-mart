4f5cba1913c630de37dfcb279e02590fe479cd59
ron <rinbar70@gmail.com>
2021-01-02 10:09:11 +0200

    Improve the PDF receipts.

--- util/receipt.js
+++ util/receipt.js
@@ -9,13 +9,14 @@ async function generateReceipt(order) {
     await order.populate('items.product').execPopulate();
     const dir = path.join(global.staticFilesDir, 'receipts');
     await fs.mkdir(dir, { recursive: true });
-    const rows = order.items.map(({ product: { name, price }, quantity }, idx) => `${idx + 1} | ${name} | ${quantity} | ${price} | ${quantity * price}`).join('\n');
+    const rows = order.items
+        .map(({ product: { name, price }, quantity }, idx) => `| ${idx + 1} | ${name} | ${quantity} | &euro;${price.toFixed(2)} | &euro;${(quantity * price).toFixed(2)} |`).join('\n');
     const totalItems = order.items.reduce((sum, { quantity }) => sum + quantity, 0);
     const totalPrice = order.items.reduce((sum, { product: { price }, quantity }) => sum + quantity * price, 0);
-    const markdown = `\\# | Product | Quantity | Price | Total
--|-|-|-|-
+    const markdown = `| # | Product | Quantity | Price | Total |
+|-|-|-|-|-|
 ${rows}
-| **Total:** | **${totalItems}** | | **${totalPrice}**`;
+| | **Total:** | **${totalItems}** | | **&euro;${totalPrice.toFixed(2)}** |`;
     log(markdown);
     await fs.writeFile(path.join(dir, `${order._id}.md`), markdown);
     log('Generating PDF...');
