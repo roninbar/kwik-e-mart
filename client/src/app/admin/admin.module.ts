1a552b919c1982fead2fdf7c3eb3d437f6940b63
ron <rinbar70@gmail.com>
2021-01-11 16:22:56 +0200

    Add the "+" button and the product details sidenav.

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { AdminRoutingModule } from './admin-routing.module';
-import { AdminPage } from './admin-page/admin.page';
-import { InventoryPage } from './inventory-page/inventory.page';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { AdminPage } from './admin-page/admin.page';
+import { AdminRoutingModule } from './admin-routing.module';
+import { InventoryPage } from './inventory-page/inventory.page';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
     AdminRoutingModule,
   ],
   declarations: [
