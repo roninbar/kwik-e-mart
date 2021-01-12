1608f676664999ad3d321307fc1348916310258a
ron <rinbar70@gmail.com>
2021-01-12 13:21:45 +0200

    Add endpoint "PUT /api/category/:cid/product/:pid".

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,7 @@ const schema = new Schema({
     name: { type: String, required: true, unique: true, trim: true },
     price: { type: Schema.Types.Decimal128, get: Number, required: true, min: 0 },
     imageUrl: { type: String, required: true },
-    categoryId: { type: Schema.Types.ObjectId, ref: 'ProductCategory' },
+    categoryId: { type: Schema.Types.ObjectId, ref: 'ProductCategory', required: true },
 }, {
     id: false,
     timestamps: true,
