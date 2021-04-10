d47b338a4a242b6022839be95c7047b1ca789920
ron <rinbar70@gmail.com>
2021-04-10 14:53:12 +0300

    Replace Bootstrap classes with Material components.

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -14,6 +15,7 @@ import { InventoryPage } from './inventory-page/inventory.page';
     CommonModule,
     FormsModule,
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatSidenavModule,
     MatTabsModule,
