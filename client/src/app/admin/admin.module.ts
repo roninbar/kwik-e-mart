1a92c6b3089ffff63ef39c940477a2b31b3e7f7d
ron <rinbar70@gmail.com>
2021-04-25 01:30:56 +0300

    Add a menu with a logout button.

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -4,6 +4,7 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -18,6 +19,7 @@ import { RootPage } from './root-page/root.page';
     MatButtonModule,
     MatCardModule,
     MatIconModule,
+    MatMenuModule,
     MatSidenavModule,
     MatTabsModule,
     MatToolbarModule,
