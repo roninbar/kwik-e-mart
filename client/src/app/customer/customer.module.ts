6c5d8fdd070a7b2669ed609b265754a6afb172d5
ron <rinbar70@gmail.com>
2021-05-02 23:28:37 +0300

    Improve the styling of the shopping cart sidenav some more (but the quantity field and the "remove" button still don't work properly).

--- client/src/app/customer/customer.module.ts
+++ client/src/app/customer/customer.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -22,6 +23,7 @@ import { CustomerRoutingModule } from './customer-routing.module';
     MatBadgeModule,
     MatButtonModule,
     MatCardModule,
+    MatDividerModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
