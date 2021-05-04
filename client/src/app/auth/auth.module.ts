e157c5cc96a59f1311042c5107bb16ba729aa8b8
ron <rinbar70@gmail.com>
2021-05-04 04:42:30 +0300

    Change "city" field from mat-select to mat-autocomplete.

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
 import { MatStepperModule } from '@angular/material/stepper';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInPage } from './sign-in-page/sign-in.page';
@@ -19,11 +19,11 @@ import { SignUpPage } from './sign-up-page/sign-up.page';
   imports: [
     CommonModule,
     FormsModule,
+    MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    MatSelectModule,
     MatStepperModule,
     AuthRoutingModule,
   ]
