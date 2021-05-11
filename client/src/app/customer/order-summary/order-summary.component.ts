63b6de022e2780d71d5f31b6c108ba489c25ba54
ron <rinbar70@gmail.com>
2021-05-11 22:35:09 +0300

    Highlight items that match the search in yellow.

--- client/src/app/customer/order-summary/order-summary.component.ts
+++ client/src/app/customer/order-summary/order-summary.component.ts
@@ -2,12 +2,18 @@ import { Component, Input, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CartService } from 'src/app/services/cart.service';
 import { OrderItem } from 'src/app/types/order-item';
+
+const Q = 'q';
 
 @Component({
   selector: 'kwik-e-mart-order-summary',
   templateUrl: './order-summary.component.html',
-  styleUrls: ['./order-summary.component.css']
+  styleUrls: ['./order-summary.component.css'],
 })
 export class OrderSummaryComponent {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -16,13 +22,42 @@ export class OrderSummaryComponent {
   @Input() dataSource!: OrderItem[];
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['name', 'quantity', 'price', 'total'];
+  public readonly displayedColumns = ['name', 'quantity', 'price', 'total'];
 
-  public totalItems(): number {
+  public readonly matchingItemIds$ = this.route.queryParamMap.pipe(
+    map((queryParamMap) =>
+      this.cartService
+        .getAllItems()
+        .filter(
+          ({ product: { name } }) =>
+            queryParamMap.has(Q) &&
+            name
+              .toLowerCase()
+              .includes(queryParamMap.get(Q)?.toLowerCase() || '')
+        )
+        .map(({ product: { _id } }) => _id)
+    )
+  );
+
+  public constructor(
+    private cartService: CartService,
+    private route: ActivatedRoute
+  ) { }
+
+  public doesMatchRx(productId: string): Observable<boolean> {
+    return this.matchingItemIds$.pipe(
+      map(ids => ids.includes(productId))
+    );
+  }
+
+  public totalQuantity(): number {
     return this.dataSource.reduce((sum, { quantity }) => sum + quantity, 0);
   }
 
   public totalPrice(): number {
-    return this.dataSource.reduce((sum, { quantity, purchasePrice }) => sum + quantity * purchasePrice, 0);
+    return this.dataSource.reduce(
+      (sum, { quantity, purchasePrice }) => sum + quantity * purchasePrice,
+      0
+    );
   }
 }
