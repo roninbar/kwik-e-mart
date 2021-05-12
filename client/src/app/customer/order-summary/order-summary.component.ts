218e094e76805df606f1b2c9cbd24ffbac190f4e
ron <rinbar70@gmail.com>
2021-05-12 16:17:48 +0300

    Bugfix: use OrderSummaryComponent's dataSource input instead of CartService.

--- client/src/app/customer/order-summary/order-summary.component.ts
+++ client/src/app/customer/order-summary/order-summary.component.ts
@@ -5,7 +5,6 @@ import { MatTable } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { CartService } from 'src/app/services/cart.service';
 import { OrderItem } from 'src/app/types/order-item';
 
 const Q = 'q';
@@ -24,23 +23,21 @@ export class OrderSummaryComponent {
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   public readonly displayedColumns = ['name', 'quantity', 'price', 'total'];
 
+  /**
+   * An {@link Observable<Array<string>>} containing the IDs of all the products whose names contain the search string.
+   */
   public readonly matchingItemIds$ = this.route.queryParamMap.pipe(
     map((queryParamMap) => {
       const q = queryParamMap.get(Q)?.toLowerCase() || '';
-      return this.cartService
-        .getAllItems()
-        .filter(
-          ({ product: { name } }) =>
-            queryParamMap.has(Q) && name.toLowerCase().includes(q)
-        )
+      return this.dataSource
+        .filter(({ product: { name } }) => queryParamMap.has(Q) && name.toLowerCase().includes(q))
         .map(({ product: { _id } }) => _id);
     })
   );
 
   public constructor(
-    private cartService: CartService,
     private route: ActivatedRoute
-  ) {}
+  ) { }
 
   public doesMatchRx(productId: string): Observable<boolean> {
     return this.matchingItemIds$.pipe(map((ids) => ids.includes(productId)));
